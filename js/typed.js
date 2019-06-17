4d51a8e30b037464a49627bfed10930560297748
ubaidkolad <bhaid023@gmail.com>
2019-06-17 19:59:03 +0530

    .

---
+++ js/typed.js
@@ -0,0 +1,8 @@
+$("document").ready(function() {
+  var typed = new Typed("#typed", {
+    strings: ["Ubaid Kolad.", "a Web Designer.", " an iPhoneographer."],
+    backSpeed: 50,
+    typeSpeed: 100,
+    loop: true
+  });
+});
